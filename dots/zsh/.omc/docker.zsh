062ca074612410b9c9c98d1ec47bd3d9e994f291
vipex <vipex@vipexsoft.com>
2026-03-16 11:13:48 +0100

    fix(zsh): fix docker compose alias

--- dots/zsh/.omc/docker.zsh
+++ dots/zsh/.omc/docker.zsh
@@ -4,7 +4,7 @@ alias dk="docker"
 alias dki="docker images"
 alias dkxxx="docker system prune -f --volumes"
 alias dkr="docker run -it --rm"
-alias dkc="docker-compose"
+alias dkc="docker compose"
 function dkrv () {
 	docker run -it --rm -v "$PWD":/ws -w /ws "${@}"
 }
